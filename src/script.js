c64c7a3765b4f2a88badb1de4e32dc8a97e1be12
J.R. Ruggiero <fleury14@comcast.net>
2018-05-07 15:47:36 -0700

    added challenges

--- src/script.js
+++ src/script.js
@@ -1,1 +1,48 @@
 document.getElementById('js-h3').innerHTML = 'I use the JavaScript stuffs';
+
+const value = 5;
+
+/* Challenge:
+1: Diplay the value of the variable as such: 'The value is ' <value>.
+2. If the value is less than 10, display the following text underneath: 'WARNING: Low number'
+*/
+
+const colors = ['red', 'blue', 'green', 'yellow'];
+
+/* Challenge:
+1: Display every color in the array on the page
+2: Display each color in the srray in a box that corresponds to its color
+3: If I change the color in the array, the color in the box should change automatically
+
+*/
+
+
+
+const fighters = [{
+    name: 'Ryu',
+    country: 'Japan',
+    style: 'Shotokan',
+    img: 'assets/ryu.jpg'
+},
+{
+    name: 'Sagat',
+    country: 'Thailand',
+    style: 'Muay Thai',
+    img: 'assets/sagat.jpg'
+},
+{
+    name: 'Guy',
+    country: 'USA',
+    style: 'Bushinryu',
+    img: 'assets/guy.jpg'
+},
+{
+    name: 'Laura',
+    country: 'Brazil',
+    style: 'Matsuda-style Jiu-Jutsu',
+    img: 'assets/laura.jpg'
+}]
+
+/* Challenge:
+Display a list of cards that goes through the array and displays each fighters name, country of origin, and fighting style, and thumbnail
+*/
